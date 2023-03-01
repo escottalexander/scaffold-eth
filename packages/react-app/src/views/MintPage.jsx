902a4d1bc02c45d8ce7b47d4a82a8625d3b536ea
escottalexander@gmail.com <escottalexander@gmail.com>
2023-03-01 08:16:25 -0500

    make button float and add description text

--- packages/react-app/src/views/MintPage.jsx
+++ packages/react-app/src/views/MintPage.jsx
@@ -26,7 +26,7 @@ export default function MintPage({
   const getBalloonsForAddress = async addr => {
     try {
       const response = await fetch(`${indexerUrl}/all/${addr}`);
-      const tokensMetadata = /* await new Promise((resolve) => resolve(exampleData)) */ await response.json();
+      const tokensMetadata = await response.json();
       setSelectedBalloon(tokensMetadata[0]);
       setMyBalloons(tokensMetadata);
     } catch (e) {
@@ -92,11 +92,10 @@ export default function MintPage({
       <div
         style={{
           zIndex: 2,
-          position: "absolute",
-          top: "100px",
           display: "flex",
           flexDirection: "column",
           flexWrap: "nowrap",
+          justifyContent: "center",
           alignContent: "center",
           alignItems: "center",
           borderRadius: "25px",
@@ -106,7 +105,17 @@ export default function MintPage({
           fontSize: 20,
         }}
       >
-        <h2>Mint a balloon to launch it on the big screen</h2>
+        {allMyBalloons && allMyBalloons.length > 0 ? (
+          <h2 style={{ marginTop: 50 }}>Mint a balloon to launch it on the big screen</h2>
+        ) : (
+          <h2>
+            AUSTIN, WRITE SOMETHING HERE. Contrary to popular belief, Lorem Ipsum is not simply random text. It has
+            roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard
+            McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin
+            words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical
+            literature, discovered the undoubtable source.
+          </h2>
+        )}
         {isSigner ? (
           <button
             className="btn-large-mobile"
@@ -122,23 +131,37 @@ export default function MintPage({
             Connect Wallet
           </button>
         )}
-        <h1 style={{ marginBottom: 0 }}>My Balloons</h1>
-        <div style={{ display: "block", background: "#ffffff30", width: "100%", height: "auto" }}>
+        {allMyBalloons && allMyBalloons.length > 0 ? (
           <>
-            <h1 style={{ fontSize: 30, margin: 0 }}>
-              {allMyBalloons && allMyBalloons.length > 1 ? "< < <  Swipe  > > >" : " "}
-            </h1>
-            <Carousel ref={carousel} afterChange={afterChange} beforeChange={onChange} dots dotPosition="bottom">
-              {showMyBalloons()}
-            </Carousel>
+            <h1 style={{ marginBottom: 0 }}>My Balloons</h1>
+            <div style={{ display: "block", background: "#ffffff30", width: "100%", height: "auto" }}>
+              <>
+                <h1 style={{ fontSize: 30, margin: 0 }}>
+                  {allMyBalloons && allMyBalloons.length > 1 ? "< < <  Swipe  > > >" : " "}
+                </h1>
+                <Carousel ref={carousel} afterChange={afterChange} beforeChange={onChange} dots dotPosition="bottom">
+                  {showMyBalloons()}
+                </Carousel>
+              </>
+            </div>
+            {selectedBalloon ? (
+              <div
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  textShadow: "2px 2px 7px #000" /* , flex: "1 1 auto", background: "#00000040" */,
+                }}
+              >
+                <h1 style={{ fontSize: 36 }}>{selectedBalloon.name}</h1>
+                <h1 style={{ fontSize: 36 }}>
+                  Launched On: {new Date(Number(selectedBalloon.launchDate)).toDateString()}
+                </h1>
+                <h1 style={{ fontSize: 36 }}>{selectedBalloon.description}</h1>
+              </div>
+            ) : (
+              ""
+            )}
           </>
-        </div>
-        {selectedBalloon ? (
-          <div style={{ width: "100%", height: "100%", flex: "1 1 auto", background: "#00000040" }}>
-            <h1 style={{ fontSize: 36 }}>{selectedBalloon.name}</h1>
-            <h1 style={{ fontSize: 36 }}>Launched On: {new Date(Number(selectedBalloon.launchDate)).toDateString()}</h1>
-            <h1 style={{ fontSize: 36 }}>{selectedBalloon.description}</h1>
-          </div>
         ) : (
           ""
         )}
